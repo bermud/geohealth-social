f57dce81e6a615172040b784db7ee8b87b94aed5
Sarah New <snew@umbc.edu>
2018-05-17 22:25:45 -0400

    Add files via upload

--- analytics/togeo_ProjectFinal.py
+++ analytics/togeo_ProjectFinal.py
@@ -16,17 +16,17 @@ except ImportError:
 
 parsed_json = json.load(open(in_file))
 output = open(out_file, 'w', encoding='utf-8')
-opener = "{'type': 'FeatureCollection',    'features': ["
+opener = '{"type": "FeatureCollection", "features": ['
 print(opener, file=output)
-beginF = '{"type": "Feature","properties" : {'
-endF = '}}'
+beginF = '{"type": "Feature", "properties" : {'
+endF = '}},'
 endAll = ']}'
-beginGeo = '}"geometry" : {'
+beginGeo = '}, "geometry" : {'
 
 tweets = parsed_json["statuses"]
 for d in tweets:
     print(beginF, file=output)
-    print('"id": {}'.format(d["id"]),file=output)
+    print('"id": {},'.format(d["id"]),file=output)
     print('"date": "{}",'.format(d["created_at"]),file=output)
     print('"text":"{}"'.format(d["text"]),file=output)
     print(beginGeo, file=output)
@@ -50,14 +50,31 @@ for d in tweets:
         cityname = d["user"]["location"]
         geolocator = Nominatim()
         gn = geocoders.GeoNames(username='ges771')
-        tweetcity = geolocator.geocode(cityname)
-        spread2 = round(random.uniform(.001,.099), 3)
-        ops = (add, sub)
-        op = random.choice(ops)
-        twlat = op(tweetcity.latitude,spread2)
-        op2 = random.choice(ops)
-        twlong = op2(tweetcity.longitude,spread2)
-        print('"type": "Point", "coordinates": [{},{}]'.format(twlong,twlat), file=output)
+        try:
+            tweetcity = geolocator.geocode(cityname)
+            spread2 = round(random.uniform(.001,.099), 3)
+            ops = (add, sub)
+            op = random.choice(ops)
+            twlat = op(tweetcity.latitude,spread2)
+            op2 = random.choice(ops)
+            twlong = op2(tweetcity.longitude,spread2)
+            print('"type": "Point", "coordinates": [{},{}]'.format(twlong,twlat), file=output)
+        except ValueError:
+            pass
+            radius = 26000
+            radiusInDegrees=radius/111300            
+            r = radiusInDegrees
+            x0 = 39.4689151
+            y0 = -77.4500538
+            u = float(random.uniform(0.0,1.0))
+            v = float(random.uniform(0.0,1.0))
+            w = r * math.sqrt(u)
+            t = 2 * math.pi * v
+            x = w * math.cos(t) 
+            y = w * math.sin(t)
+            xLat  = x + x0
+            yLong = y + y0
+            print('"type": "Point", "coordinates": [{},{}]'.format(yLong,xLat), file=output)					
 #Twitter uses a combination of device/gps coordinates, user provided profile location, and network/ip address location to determine tweets that fit within the geocode search parameter.  So we will have some tweets with no useful location data.  What then? I've made a random point in our original radius.
     else:
         radius = 26000
